054b89b0df355d2a46c5a2c7f829277b2fce2e68
Murad Malik <muradmalik7@gmail.com>
2019-02-12 01:59:36 +0500

    bugs resolved

--- coming-soon.php
+++ coming-soon.php
@@ -63,21 +63,5 @@
         </div>
     </div>
      <?php include "footer.php" ?>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/menumaker.js"></script>
-
-    <!-- sticky header -->
-    <script type="text/javascript" src="js/jquery.sticky.js"></script>
-    <script type="text/javascript" src="js/sticky-header.js"></script>
-    <!-- slider script -->
-    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
-    <script type="text/javascript" src="js/slider-carousel.js"></script>
-    <script type="text/javascript" src="js/service-carousel.js"></script>
-    <!-- Back to top script -->
-    <script src="js/back-to-top.js" type="text/javascript"></script>
 </body>
 </html>
